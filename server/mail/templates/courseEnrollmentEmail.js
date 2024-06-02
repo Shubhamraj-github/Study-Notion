5e28d37d36435d7f3444d807b22e869c9284b6ed
Shubhamraj-github <srshubhamraj98841@gmail.com>
2024-06-02 10:53:02 +0530

    Changes in the templete

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-bhavesh.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="http://localhost:3000"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                     alt="StudyNotion Logo"></a>
             <div class="message">Course Registration Confirmation</div>
             <div class="body">
